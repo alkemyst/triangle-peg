b9720ea4ac424dad406f286db14406bca867154e
Stefano Mersi <stefano.mersi@gmail.com>
2020-05-06 19:58:40 +0200

    Ready to optimize

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 all: baseProblem.exe flexiSearch.exe flexiSearchFast.exe
 
+# Add -pg for gprof
 %.o: %.cpp %.h
-	g++ -Werror -fmax-errors=1 -O3 -c $< -o $@
+	g++ -pg -Werror -fmax-errors=1 -O3 -c $< -o $@
 
 %.exe: %.cpp Move.o Coordinate.o Triangle.o
-	g++ -Werror -fmax-errors=1 -O3 $< Move.o Coordinate.o Triangle.o -o $@
+	g++ -pg -Werror -fmax-errors=1 -O3 $< Move.o Coordinate.o Triangle.o -o $@
+
+profile:
+	./flexiSearch.exe 5 1
+	gprof ./flexiSearchFast.exe | gprof2dot/gprof2dot.py | dot -Tsvg -o output.svg
 
 clean:
 	rm -f *~ *.exe *.o
---
+++ flexiSearchFast.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include "Triangle.h"
+#include "Move.h"
+
+int main(int argc, char* argv[]) {
+  if (argc!=3) {
+    std::cerr << "Syntax: " << argv[0] << " side_size marble_to_remove" << std::endl;
+    return -1;
+  }
+
+  int side = atoi(argv[1]);
+  int toRemove = atoi(argv[2]);
+  
+  Triangle myTriangle(side);
+  myTriangle.remove(toRemove);
+  
+  myTriangle.print();
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  MoveVector moveVector;
+  MoveVector bestMoves;
+  int bestScore = myTriangle.nSpots;
+  Triangle::exploreGameOptimal(myTriangle, moveVector, bestMoves, bestScore);
+
+  if (bestScore!=myTriangle.nSpots) {
+    std::cerr << "Solved. Best score in " << bestScore << std::endl;
+    Triangle::listMoves(bestMoves);
+  } else {
+    std::cerr << "Not_possible" << std::endl;    
+  }
+  
+  return 0;
+};
